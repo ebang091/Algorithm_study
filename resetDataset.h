64f5b19efe835c88fb06c6e358441aee05d5d9e2
ebang091 <108053947+ebang091@users.noreply.github.com>
2022-08-01 19:22:06 +0900

    Create resetDataset.h

---
+++ resetDataset.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define SHUFFLE_COUNT 100
+
+#define RANDOM 0
+#define ARRANGED 1
+#define REVERSED 2
+
+void resetDataset(int[], int, int);
